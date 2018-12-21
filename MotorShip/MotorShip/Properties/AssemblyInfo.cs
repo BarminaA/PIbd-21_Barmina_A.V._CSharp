f13bd350b613518bb46949b2ced55a5d7197a122
Alexandra <alexandrabarmina99@gmail.com>
2018-12-21 16:45:33 +0400

    Lab_01

---
+++ MotorShip/MotorShip/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("MotorShip")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MotorShip")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("71a0c843-17ef-444d-b223-3fc6e0f5ddcc")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MotorShip/MotorShip/Ship.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorShip
+{
+    class Ship
+    {
+        private float _startPosX;
+        private float _startPosY;
+        private int _pictureWidth;
+        private int _pictureHeight;
+        private const int shipWidth = 100;
+        private const int shipHeight = 80;
+        public int MaxSpeed { private set; get; }
+        public float Weight { private set; get; }
+        public Color MainColor { private set; get; }
+        public Color DopColor { private set; get; }
+
+        public Ship(int maxSpeed, float weight, Color mainColor, Color dopColor)
+        {
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            MainColor = mainColor;
+            DopColor = dopColor;
+
+        }
+
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            _startPosX = x;
+            _startPosY = y;
+            _pictureWidth = width;
+            _pictureHeight = height;
+        }
+
+        public void MoveShip(Direction direction)
+        {
+            float step = MaxSpeed * 100 / Weight; switch (direction)
+            {
+                case Direction.Right:
+                    if (_startPosX + step < _pictureWidth - shipWidth)
+                    {
+                        _startPosX += step;
+                    }
+                    break;
+                case Direction.Left:
+                    if (_startPosX - step > 0)
+                    {
+                        _startPosX -= step;
+                    }
+                    break;
+                case Direction.Up:
+                    if (_startPosY - step > 0)
+                    {
+                        _startPosY -= step;
+                    }
+                    break;
+                case Direction.Down:
+                    if (_startPosY + step < _pictureHeight - shipHeight)
+                    {
+                        _startPosY += step;
+                    }
+                    break;
+            }
+        }
+
+        public void DrawShip(Graphics g)
+        {
+            Pen pen = new Pen(MainColor);
+            g.DrawLine(pen, _startPosX + 40, _startPosY - 20, _startPosX + 130, _startPosY - 20);
+            g.DrawLine(pen, _startPosX + 60, _startPosY + 10, _startPosX + 110, _startPosY + 10);
+            g.DrawLine(pen, _startPosX + 60, _startPosY + 10, _startPosX + 40, _startPosY - 20);
+            g.DrawLine(pen, _startPosX + 130, _startPosY - 20, _startPosX + 110, _startPosY + 10);
+
+            g.DrawRectangle(pen, _startPosX + 60, _startPosY - 40, 50, 20);
+            g.DrawRectangle(pen, _startPosX + 70, _startPosY - 55, 10, 15);
+            g.DrawRectangle(pen, _startPosX + 90, _startPosY - 55, 10, 15);
+
+            g.DrawEllipse(pen, _startPosX + 80, _startPosY - 35, 10, 10);
+            g.DrawEllipse(pen, _startPosX + 60, _startPosY - 35, 10, 10);
+            g.DrawEllipse(pen, _startPosX + 100, _startPosY - 35, 10, 10);
+            g.DrawEllipse(pen, _startPosX + 50, _startPosY - 15, 10, 10);
+            g.DrawEllipse(pen, _startPosX + 70, _startPosY - 15, 10, 10);
+            g.DrawEllipse(pen, _startPosX + 90, _startPosY - 15, 10, 10);
+            g.DrawEllipse(pen, _startPosX + 110, _startPosY - 15, 10, 10);
+
+            Brush brush = new SolidBrush(DopColor);
+            g.FillEllipse(brush, _startPosX + 80, _startPosY - 35, 10, 10);
+            g.FillEllipse(brush, _startPosX + 60, _startPosY - 35, 10, 10);
+            g.FillEllipse(brush, _startPosX + 100, _startPosY - 35, 10, 10);
+            g.FillEllipse(brush, _startPosX + 50, _startPosY - 15, 10, 10);
+            g.FillEllipse(brush, _startPosX + 70, _startPosY - 15, 10, 10);
+            g.FillEllipse(brush, _startPosX + 90, _startPosY - 15, 10, 10);
+            g.FillEllipse(brush, _startPosX + 110, _startPosY - 15, 10, 10);
+
+            Brush brushSmoke = new SolidBrush(MainColor);
+            g.FillEllipse(brushSmoke, _startPosX + 50, _startPosY - 65, 15, 10);
+            g.FillEllipse(brushSmoke, _startPosX + 55, _startPosY - 60, 15, 10);
+            g.FillEllipse(brushSmoke, _startPosX + 70, _startPosY - 65, 15, 10);
+            g.FillEllipse(brushSmoke, _startPosX + 75, _startPosY - 60, 15, 10);
+        }
+    }
+}
